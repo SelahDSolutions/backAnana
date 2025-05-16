3e3f91317c728cf83fd2e6d48389c8eb012d4857
CinRigoniG <rigonicinthia@gmail.com>
2025-05-16 10:08:46 -0300

    first commit

---
+++ src/main/java/com/example/backAnana/BackAnanaApplication.java
@@ -0,0 +1,13 @@
+package com.example.backAnana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackAnanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackAnanaApplication.class, args);
+	}
+
+}
