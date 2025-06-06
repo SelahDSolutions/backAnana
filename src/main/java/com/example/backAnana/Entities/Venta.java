c045193b0a4bbbfae1700bce92c7c9cc48e28b76
CinRigoniG <rigonicinthia@gmail.com>
2025-06-06 09:14:11 -0300

    Estado venta

--- src/main/java/com/example/backAnana/Entities/Venta.java
+++ src/main/java/com/example/backAnana/Entities/Venta.java
@@ -1,5 +1,6 @@
 package com.example.backAnana.Entities;
 
+import com.example.backAnana.Entities.Enums.EstadoVenta;
 import com.example.backAnana.Entities.Enums.FormaPago;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
@@ -23,6 +24,7 @@ public class Venta extends Base{
     private Double total;
     private FormaPago formaPago;
     private Double envio;
+    private EstadoVenta estadoVenta;
 
     @OneToMany(mappedBy = "venta", cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
     @Builder.Default
